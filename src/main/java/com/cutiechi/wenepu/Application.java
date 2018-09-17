daf53e098a275d6b01f48c4ccd7bdd34934283d1
cutiechi <chi@cutie.ooo>
2018-09-17 11:14:09 +0800

    refactor: rename application main class

--- src/main/java/com/cutiechi/wenepu/Application.java
+++ src/main/java/com/cutiechi/wenepu/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GradleSpringBootWenepuApplication {
+public class Application {
 
     public static void main (String[] args) {
-        SpringApplication.run(GradleSpringBootWenepuApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
