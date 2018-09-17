e22fe0fb85216d3f69e32d70b828a5a5e6b67fb0
cutiechi <chi@cutie.ooo>
2018-09-17 10:51:52 +0800

    initial commit

---
+++ src/main/java/com/cutiechi/wenepu/GradleSpringBootWenepuApplication.java
@@ -0,0 +1,12 @@
+package com.cutiechi.wenepu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringBootWenepuApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(GradleSpringBootWenepuApplication.class, args);
+    }
+}
