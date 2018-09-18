5ac20335037aabe929a8b6f88e3b8dec9968ce1c
cutiechi <chi@cutie.ooo>
2018-09-19 00:13:11 +0800

    feat(handler): add global exception handler class

---
+++ src/main/java/com/cutiechi/wenepu/handler/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.cutiechi.wenepu.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理类
+ *
+ * @author Cutie Chi
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
