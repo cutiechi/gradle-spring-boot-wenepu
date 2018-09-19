1cd3fd433a396cdc70d2897a4f6220f1fd0969ab
cutiechi <chi@cutie.ooo>
2018-09-19 10:35:28 +0800

    feat(exception): add nepu server error exception

---
+++ src/main/java/com/cutiechi/wenepu/exception/NepuServerErrorException.java
@@ -0,0 +1,13 @@
+package com.cutiechi.wenepu.exception;
+
+/**
+ * 东北石油大学教务管理系统服务器错误异常
+ *
+ * @author Cutie Chi
+ */
+public class NepuServerErrorException extends Exception {
+
+    public NepuServerErrorException (String message) {
+        super(message);
+    }
+}
