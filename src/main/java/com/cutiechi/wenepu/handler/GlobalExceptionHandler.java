ed35f2b10308554d16e5243e154effbf9987c22b
cutiechi <chi@cutie.ooo>
2018-09-19 10:47:40 +0800

    feat(handler): add nepu server error exception handle method

--- src/main/java/com/cutiechi/wenepu/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cutiechi/wenepu/handler/GlobalExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.cutiechi.wenepu.handler;
 
+import com.cutiechi.wenepu.exception.NepuServerErrorException;
+import com.cutiechi.wenepu.model.dto.JsonResponse;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
@@ -10,4 +15,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
+    /**
+     * 东北石油大学教务管理系统服务器错误异常处理方法
+     *
+     * @param exception 东北石油大学教务管理系统服务器错误异常对象
+     * @return JSON 响应
+     */
+    @ExceptionHandler(NepuServerErrorException.class)
+    @ResponseBody
+    public JsonResponse nepuServerErrorExceptionHandle (NepuServerErrorException exception) {
+        return new JsonResponse(50000, exception.getMessage());
+    }
 }
