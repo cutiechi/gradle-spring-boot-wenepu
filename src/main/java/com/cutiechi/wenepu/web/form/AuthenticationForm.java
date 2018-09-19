e16e9d1e4a2ba515816d79bb2d5cd9165faa9be3
cutiechi <chi@cutie.ooo>
2018-09-19 14:28:28 +0800

    feat(form): add authentication form all args constructor

--- src/main/java/com/cutiechi/wenepu/web/form/AuthenticationForm.java
+++ src/main/java/com/cutiechi/wenepu/web/form/AuthenticationForm.java
@@ -1,6 +1,6 @@
 package com.cutiechi.wenepu.web.form;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +11,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@AllArgsConstructor
 public class AuthenticationForm {
 
     /**
