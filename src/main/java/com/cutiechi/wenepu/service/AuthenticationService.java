63fd3f290e83f1328d1db440603d0b08367c634d
cutiechi <chi@cutie.ooo>
2018-09-19 14:18:16 +0800

    feat(service): add get web token method

--- src/main/java/com/cutiechi/wenepu/service/AuthenticationService.java
+++ src/main/java/com/cutiechi/wenepu/service/AuthenticationService.java
@@ -1,5 +1,9 @@
 package com.cutiechi.wenepu.service;
 
+import com.cutiechi.wenepu.exception.NepuServerErrorException;
+import com.cutiechi.wenepu.model.dto.ServiceResult;
+import com.cutiechi.wenepu.web.form.AuthenticationForm;
+
 /**
  * 鉴权业务逻辑接口
  *
@@ -7,4 +11,13 @@ package com.cutiechi.wenepu.service;
  */
 public interface AuthenticationService {
 
+
+    /**
+     * 获取 Web Token, 该 Token 本质为 JSESSIONID, 请求东北石油大学教务系统时需该 Token
+     *
+     * @param authenticationForm 鉴权表单
+     * @return 附带 Web Token 的业务逻辑结果数据传输对象
+     * @throws NepuServerErrorException 东北石油大学教务管理系统服务器错误异常
+     */
+    ServiceResult getWebToken (AuthenticationForm authenticationForm) throws NepuServerErrorException;
 }
--- src/main/java/com/cutiechi/wenepu/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/cutiechi/wenepu/service/impl/AuthenticationServiceImpl.java
@@ -1,13 +1,19 @@
 package com.cutiechi.wenepu.service.impl;
 
 import com.cutiechi.wenepu.exception.NepuServerErrorException;
+import com.cutiechi.wenepu.model.dto.ServiceResult;
 import com.cutiechi.wenepu.service.AuthenticationService;
 import com.cutiechi.wenepu.util.VerificationCodeImageUtils;
+import com.cutiechi.wenepu.web.form.AuthenticationForm;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -129,5 +135,112 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             throw new NepuServerErrorException("服务器错误，获取 Web Token 失败！");
         }
         return webVerification;
+    }
+
+
+    /**
+     * 获取 Web Token, 该 Token 本质为 JSESSIONID, 请求东北石油大学教务系统时需该 Token
+     *
+     * @param authenticationForm 鉴权表单
+     * @return 附带 Web Token 的业务逻辑结果数据传输对象
+     * @throws NepuServerErrorException 东北石油大学教务管理系统服务器错误异常
+     */
+    @Override
+    public ServiceResult getWebToken (AuthenticationForm authenticationForm) throws NepuServerErrorException {
+
+        // 获取 Web 验证信息字符串数组
+        final String[] webVerification = getWebVerification();
+
+        // Web 验证信息数组的第一个元素为验证码
+        final String verificationCode = webVerification[0];
+
+        // Web 验证信息数组的第二个元素为 Web Token
+        final String webToken = webVerification[1];
+
+        System.out.println(webToken);
+        System.out.println(verificationCode);
+        // 从鉴权表单中获取学号
+        final String studentNumber = authenticationForm.getStudentNumber();
+
+        // 从鉴权表单中获取学生密码
+        final String studentPassword = authenticationForm.getStudentPassword();
+
+        // 获取 Web Token 请求的东北石油大学教务管理系统 URL 地址
+        final String LOGIN_URL = "http://jwgl.nepu.edu.cn/Logon.do";
+
+        // 登录成功后第一次重定向的 URL 地址
+        final String FIRST_REDIRECT_URL = "http://jwgl.nepu.edu.cn/framework/main.jsp";
+
+        // 登录成功后第二次重定向的 URL 地址
+        final String SECOND_REDIRECT_URL = "http://jwgl.nepu.edu.cn/Logon.do";
+
+        // 请求所携带 Cookie 的 KEY
+        final String COOKIE_KEY = "JSESSIONID";
+
+        // 请求方法的 KEY
+        final String METHOD_KEY = "method";
+
+        // 请求方法的值
+        final String METHOD_VALUE = "logon";
+
+        // 第二次重定向请求方法的值
+        final String SECOND_REDIRECT_METHOD_VALUE = "logonBySSO";
+
+        // 请求携带数据学生编号的 KEY
+        final String STUDENT_NUMBER_KEY = "USERNAME";
+
+        // 请求携带数据学生密码的 KEY
+        final String STUDENT_PASSWORD_KEY = "PASSWORD";
+
+        // 请求携带数据验证码的 KEY
+        final String VERIFICATION_CODE_KEY = "RANDOMCODE";
+
+        // 请求超时时间, 当前为 10 秒
+        final int TIMEOUT_MILLIS = 10 * 1000;
+
+        //实例化 Jsoup 连接对象
+        final Connection connection = Jsoup.connect(LOGIN_URL)
+            .data(METHOD_KEY, METHOD_VALUE)
+            .data(STUDENT_NUMBER_KEY, studentNumber)
+            .data(STUDENT_PASSWORD_KEY, studentPassword)
+            .data(VERIFICATION_CODE_KEY, verificationCode)
+            .cookie(COOKIE_KEY, webToken)
+            .timeout(TIMEOUT_MILLIS);
+        try {
+
+            // 获取请求后的 Jsoup Document 对象, 此时可能会发生 IOException
+            final Document document = connection.post();
+
+            // 获取文档的标题
+            final String title = document.title();
+
+            System.out.println(title);
+
+            // 当标题长度不为 21 的时候登录东北石油大学教务管理系统成功
+            if (21 != title.length()) {
+
+                // 登录成功后需进行两次重定向
+                Jsoup.connect(FIRST_REDIRECT_URL)
+                    .cookie(COOKIE_KEY, webToken)
+                    .timeout(TIMEOUT_MILLIS)
+                    .post();
+                Jsoup.connect(SECOND_REDIRECT_URL)
+                    .data(METHOD_KEY, SECOND_REDIRECT_METHOD_VALUE)
+                    .cookie(COOKIE_KEY, webToken)
+                    .timeout(TIMEOUT_MILLIS)
+                    .post();
+
+                // 返回携带 Web Token 的业务逻辑结果对象
+                return ServiceResult.success("获取 Web Token 成功！", webToken);
+            } else {
+
+                // 当标题长度为 21 的时候登录失败
+                return ServiceResult.fail("学号或密码错误，获取 Web Token 失败！");
+            }
+        } catch (IOException e) {
+
+            // 当发生 IOException 时抛出东北石油大学服务器错误异常
+            throw new NepuServerErrorException("服务器错误，获取 Web Token 失败！");
+        }
     }
 }
