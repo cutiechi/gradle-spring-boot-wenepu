243f122efbd1456c05b351649f23703569eb432c
cutiechi <chi@cutie.ooo>
2018-09-20 18:55:17 +0800

    refactor(service): use authentication config refactor authentication service

--- src/main/java/com/cutiechi/wenepu/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/cutiechi/wenepu/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cutiechi.wenepu.service.impl;
 
+import com.cutiechi.wenepu.config.AuthenticationConfig;
 import com.cutiechi.wenepu.exception.NepuServerErrorException;
 import com.cutiechi.wenepu.model.dto.ServiceResult;
 import com.cutiechi.wenepu.service.AuthenticationService;
@@ -33,6 +34,10 @@ import java.io.IOException;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+    /**
+     * 鉴权配置类对象
+     */
+    private final AuthenticationConfig authenticationConfig;
 
     /**
      * 获取 Web 验证信息时识别验证码图片的工具类
@@ -43,10 +48,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
      * Spring 自动注入依赖
      *
      * @param verificationCodeImageUtils 获取 Web 验证信息时识别验证码图片的工具类
+     * @param authenticationConfig       鉴权配置类对象
      */
     @Autowired
-    public AuthenticationServiceImpl (VerificationCodeImageUtils verificationCodeImageUtils) {
+    public AuthenticationServiceImpl (VerificationCodeImageUtils verificationCodeImageUtils, AuthenticationConfig authenticationConfig) {
         this.verificationCodeImageUtils = verificationCodeImageUtils;
+        this.authenticationConfig = authenticationConfig;
     }
 
     /**
@@ -59,19 +66,13 @@ public class AuthenticationServiceImpl implements AuthenticationService {
      */
     private String[] getWebVerification () throws NepuServerErrorException {
 
-        // 获取 Web 验证信息所请求的 URL
-        final String WEB_VERIFICATION_URL = "http://jwgl.nepu.edu.cn/verifycode.servlet";
-
-        // JSESSIONID 在响应头部中的 Key
-        final String HEADER_JSESSIONID_KEY = "Set-Cookie";
-
         // 实例化 OkHttpClient 对象
         final OkHttpClient client = new OkHttpClient();
 
         // 实例化请求对象
         final Request request = new Request
             .Builder()
-            .url(WEB_VERIFICATION_URL)
+            .url(authenticationConfig.getWebVerificationUrl())
             .get()
             .build();
 
@@ -91,28 +92,28 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 // 响应状态码为 200 时继续
                 if (HttpStatus.OK.value() == response.code()) {
                     // 实例化响应 Body 对象
-                    ResponseBody body = response.body();
+                    final ResponseBody body = response.body();
 
                     // Body 不为空
                     if (null != body) {
 
                         // 从响应 Body 的 字节流中读取验证码图片到 Buffered Image 对象中
-                        BufferedImage verificationCodeImage = ImageIO.read(body.byteStream());
+                        final BufferedImage verificationCodeImage = ImageIO.read(body.byteStream());
 
                         // 识别验证码图片
-                        String verificationCode = verificationCodeImageUtils.recognition(verificationCodeImage);
+                        final String verificationCode = verificationCodeImageUtils.recognition(verificationCodeImage);
 
                         // 由于这个验证码识别工具的可能会错误的识别 m, 只有不含 m 时继续
                         if (!verificationCode.contains("m")) {
 
                             // 从响应头部中读取 JSESSIONID
-                            String JSESSIONID = response.header(HEADER_JSESSIONID_KEY);
+                            final String JSESSIONID = response.header(authenticationConfig.getHeaderJSessionIdKey());
 
                             // JSESSIONID 不为空继续
                             if (null != JSESSIONID) {
 
                                 // 这里将从 JSESSIONID 中截取的部分字符串当做 Web Token, 其实本质还是 JSESSIONID
-                                String webToken = JSESSIONID.substring(11, 43);
+                                final String webToken = JSESSIONID.substring(11, 43);
 
                                 // 将验证码和 Web Token 存放到 web 验证信息中
                                 webVerification[0] = verificationCode;
@@ -146,84 +147,48 @@ public class AuthenticationServiceImpl implements AuthenticationService {
      * @throws NepuServerErrorException 东北石油大学教务管理系统服务器错误异常
      */
     @Override
-    public ServiceResult getWebToken (AuthenticationForm authenticationForm) throws NepuServerErrorException {
+    public ServiceResult getWebToken (final AuthenticationForm authenticationForm) throws NepuServerErrorException {
 
         // 获取 Web 验证信息字符串数组
         final String[] webVerification = getWebVerification();
 
-        // Web 验证信息数组的第一个元素为验证码
-        final String verificationCode = webVerification[0];
-
         // Web 验证信息数组的第二个元素为 Web Token
         final String webToken = webVerification[1];
 
-        // 从鉴权表单中获取学号
-        final String studentNumber = authenticationForm.getStudentNumber();
+        // 从配置类中获取请求方法的 Key
+        final String methodKey = authenticationConfig.getMethodKey();
 
-        // 从鉴权表单中获取学生密码
-        final String studentPassword = authenticationForm.getStudentPassword();
+        // 从配置类对象中获取请求所携带 Cookie 的 KEY
+        final String cookieKey = authenticationConfig.getCookieKey();
 
-        // 获取 Web Token 请求的东北石油大学教务管理系统 URL 地址
-        final String LOGIN_URL = "http://jwgl.nepu.edu.cn/Logon.do";
-
-        // 登录成功后第一次重定向的 URL 地址
-        final String FIRST_REDIRECT_URL = "http://jwgl.nepu.edu.cn/framework/main.jsp";
-
-        // 登录成功后第二次重定向的 URL 地址
-        final String SECOND_REDIRECT_URL = "http://jwgl.nepu.edu.cn/Logon.do";
-
-        // 请求所携带 Cookie 的 KEY
-        final String COOKIE_KEY = "JSESSIONID";
-
-        // 请求方法的 KEY
-        final String METHOD_KEY = "method";
-
-        // 请求方法的值
-        final String METHOD_VALUE = "logon";
-
-        // 第二次重定向请求方法的值
-        final String SECOND_REDIRECT_METHOD_VALUE = "logonBySSO";
-
-        // 请求携带数据学生编号的 KEY
-        final String STUDENT_NUMBER_KEY = "USERNAME";
-
-        // 请求携带数据学生密码的 KEY
-        final String STUDENT_PASSWORD_KEY = "PASSWORD";
-
-        // 请求携带数据验证码的 KEY
-        final String VERIFICATION_CODE_KEY = "RANDOMCODE";
-
-        // 请求超时时间, 当前为 10 秒
-        final int TIMEOUT_MILLIS = 10 * 1000;
+        // 从配置类对象中获取请求超时时间
+        final Integer timeout = authenticationConfig.getTimeout();
 
         //实例化 Jsoup 连接对象
-        final Connection connection = Jsoup.connect(LOGIN_URL)
-            .data(METHOD_KEY, METHOD_VALUE)
-            .data(STUDENT_NUMBER_KEY, studentNumber)
-            .data(STUDENT_PASSWORD_KEY, studentPassword)
-            .data(VERIFICATION_CODE_KEY, verificationCode)
-            .cookie(COOKIE_KEY, webToken)
-            .timeout(TIMEOUT_MILLIS);
+        final Connection connection = Jsoup.connect(authenticationConfig.getLoginUrl())
+            .data(methodKey, authenticationConfig.getMethodValue())
+            .data(authenticationConfig.getStudentNumberKey(), authenticationForm.getStudentNumber())
+            .data(authenticationConfig.getStudentPasswordKey(), authenticationForm.getStudentPassword())
+            .data(authenticationConfig.getVerificationCodeKey(), webVerification[0])
+            .cookie(cookieKey, webToken)
+            .timeout(timeout);
         try {
 
             // 获取请求后的 Jsoup Document 对象, 此时可能会发生 IOException
             final Document document = connection.post();
 
-            // 获取文档的标题
-            final String title = document.title();
-
             // 当标题长度不为 21 的时候登录东北石油大学教务管理系统成功
-            if (21 != title.length()) {
+            if (authenticationConfig.getErrorTitleLength() != document.title().length()) {
 
                 // 登录成功后需进行两次重定向
-                Jsoup.connect(FIRST_REDIRECT_URL)
-                    .cookie(COOKIE_KEY, webToken)
-                    .timeout(TIMEOUT_MILLIS)
+                Jsoup.connect(authenticationConfig.getFirstRedirectUrl())
+                    .cookie(cookieKey, webToken)
+                    .timeout(timeout)
                     .post();
-                Jsoup.connect(SECOND_REDIRECT_URL)
-                    .data(METHOD_KEY, SECOND_REDIRECT_METHOD_VALUE)
-                    .cookie(COOKIE_KEY, webToken)
-                    .timeout(TIMEOUT_MILLIS)
+                Jsoup.connect(authenticationConfig.getSecondRedirectUrl())
+                    .data(methodKey, authenticationConfig.getSecondRedirectMethodValue())
+                    .cookie(cookieKey, webToken)
+                    .timeout(timeout)
                     .post();
 
                 // 返回携带 Web Token 的业务逻辑结果对象
