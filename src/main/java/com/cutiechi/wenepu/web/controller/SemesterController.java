7cda3e1c862f8456e458a52c200a77d5507b6733
cutiechi <chi@cutie.ooo>
2018-09-20 17:40:42 +0800

    feat(controller): add semester controller

---
+++ src/main/java/com/cutiechi/wenepu/web/controller/SemesterController.java
@@ -0,0 +1,15 @@
+package com.cutiechi.wenepu.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 开课学期控制器
+ *
+ * @author Cutie Chi
+ */
+@RequestMapping("/semesters")
+@RestController
+public final class SemesterController {
+
+}
