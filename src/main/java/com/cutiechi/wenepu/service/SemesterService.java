6a3afb965b36b083976af74c39c0f57ca1358025
cutiechi <chi@cutie.ooo>
2018-09-20 16:49:59 +0800

    feat(service): add semester service interface and impl

---
+++ src/main/java/com/cutiechi/wenepu/service/SemesterService.java
@@ -0,0 +1,10 @@
+package com.cutiechi.wenepu.service;
+
+/**
+ * 开课学期业务逻辑接口
+ *
+ * @author Cutie Chi
+ */
+public interface SemesterService {
+
+}
---
+++ src/main/java/com/cutiechi/wenepu/service/impl/SemesterServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cutiechi.wenepu.service.impl;
+
+import com.cutiechi.wenepu.service.SemesterService;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 开课学期业务逻辑实现类
+ *
+ * @author Cutie Chi
+ */
+@Service
+public class SemesterServiceImpl implements SemesterService {
+
+}
