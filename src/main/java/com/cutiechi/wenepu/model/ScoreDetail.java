39f80a055bdaec3bf2779309266a25650a8704f3
cutiechi <chi@cutie.ooo>
2018-09-22 20:54:57 +0800

    feat(model): add score detail model

---
+++ src/main/java/com/cutiechi/wenepu/model/ScoreDetail.java
@@ -0,0 +1,44 @@
+package com.cutiechi.wenepu.model;
+
+/**
+ * 成绩详情
+ *
+ * @author Cutie Chi
+ */
+public class ScoreDetail {
+
+    /**
+     * 平时成绩
+     */
+    private String dailyScore;
+
+    /**
+     * 平时成绩比例
+     */
+    private String dailyScoreRatio;
+
+    /**
+     * 期中成绩
+     */
+    private String midtermScore;
+
+    /**
+     * 其中成绩比例
+     */
+    private String midtermScoreRatio;
+
+    /**
+     * 期末成绩
+     */
+    private String endtermScore;
+
+    /**
+     * 期末成绩比例
+     */
+    private String endtermScoreRatio;
+
+    /**
+     * 总成绩
+     */
+    private String totalScore;
+}
