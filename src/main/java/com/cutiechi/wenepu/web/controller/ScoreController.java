4e81b35f645c5c81fb683f6331aec56d9036448a
cutiechi <chi@cutie.ooo>
2018-09-22 20:40:35 +0800

    feat(controller): add score controller

---
+++ src/main/java/com/cutiechi/wenepu/web/controller/ScoreController.java
@@ -0,0 +1,15 @@
+package com.cutiechi.wenepu.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 成绩控制器
+ *
+ * @author Cutie Chi
+ */
+@RequestMapping("/scores")
+@RestController
+public class ScoreController {
+
+}
