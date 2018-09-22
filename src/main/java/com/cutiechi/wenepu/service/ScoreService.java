9ae1a6227b9368a6e0c70d1aae1208c4bb8ab107
cutiechi <chi@cutie.ooo>
2018-09-22 20:23:54 +0800

    feat(service): add score service interface and impl

---
+++ src/main/java/com/cutiechi/wenepu/service/ScoreService.java
@@ -0,0 +1,10 @@
+package com.cutiechi.wenepu.service;
+
+/**
+ * 成绩业务逻辑接口
+ *
+ * @author Cutie Chi
+ */
+public interface ScoreService {
+
+}
---
+++ src/main/java/com/cutiechi/wenepu/service/impl/ScoreServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cutiechi.wenepu.service.impl;
+
+import com.cutiechi.wenepu.service.ScoreService;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 成绩业务逻辑实现类
+ */
+@Service
+public class ScoreServiceImpl implements ScoreService {
+
+}
